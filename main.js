5e3d5753397fdbf21f1122a654590f65a0a5f162
kommune <kommune.000@gmail.com>
2018-04-23 23:10:22 +0800

    Add proof of work

--- main.js
+++ main.js
@@ -7,16 +7,26 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
     createGenesisBlock(){
@@ -29,7 +39,7 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -51,10 +61,10 @@ class Blockchain{
 }
 
 let kyCoin = new Blockchain();
+
+console.log('Mining block 1...');
 kyCoin.addBlock(new Block(1, "22/04/2018", {amount: 4}));
+
+console.log('Mining block 2...');
 kyCoin.addBlock(new Block(2, "23/04/2018", {amount: 10}));
 
-console.log('Is blockchain valid?' + kyCoin.isChainValid());
-
-kyCoin.chain[1].data = {amount: 100}
-console.log('Is blockchain valid?' + kyCoin.isChainValid());
